c5ea9b56cac64eb337e1675f37a490f65018de6c
Jianrong Yu <jianrong.yu@sap.com>
2019-12-27 12:01:31 -0800

    Fix a less-plugin issue for customize-cra

--- config-overrides.js
+++ config-overrides.js
@@ -22,5 +22,24 @@ module.exports = override(
       'border-radius-base': '2px',
     },
   }),
+  config => {
+    function look4localIdentName(node) {
+      if ('localIdentName' in node) {
+        return node;
+      }
+      for (const key in node) {
+        if (node[key] instanceof Object) {
+          const n = look4localIdentName(node[key]);
+          if (n) {
+            return n;
+          }
+        }
+      }
+    }
+    const node = look4localIdentName(config);
+    node.modules = { localIdentName: node.localIdentName };
+    delete node.localIdentName;
+    return config;
+  },
   enableEslintTypescript(),
 );
